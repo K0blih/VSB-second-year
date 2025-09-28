ec5dd21c6ad8f621e6332136447da0792aa1ee25
K0blih <103500657+K0blih@users.noreply.github.com>
2025-09-28 14:39:59 +0200

    Add files via upload

---
+++ CSI/ClassArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp1_cv2
+{
+    internal class ClassArray
+    {
+        public void Run()
+        {
+            CompositeBrick[] bricks = new CompositeBrick[2];
+            bricks[0] = CreateT(1, 1);
+            bricks[1] = CreateT(5, 5);
+        }
+
+        public CompositeBrick CreateT(int x, int y)
+        {
+            CompositeBrick b = new CompositeBrick();
+            b.X = x;
+            b.Y = y;
+            b.bricks = new Brick[4];
+            b.bricks[0] = new Brick();
+            b.bricks[0].X = 0;
+            b.bricks[0].Y = 0;
+
+            b.bricks[1] = new Brick() {X = 1, Y = 0};
+            b.bricks[2] = new Brick() { X = 2, Y = 0 };
+            b.bricks[3] = new Brick() { X = 1, Y = 1 };
+
+            return b;
+        }
+    }
+}
---
+++ CSI/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Csharp1_cv2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            JaggedArray impl2 = new JaggedArray();
+            impl2.Run();
+
+            //MultidimensionalArray impl1 = new MultidimensionalArray();
+            //impl1.Run();
+        }
+    }
+}
